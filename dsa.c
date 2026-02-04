5a0062f1e3e9d9c037cec064067457816221325c
iman <mainaemmanuelmk@gmail.com>
2026-02-04 21:13:37 +0300

    Array of structs

--- dsa.c
+++ dsa.c
@@ -9,14 +9,17 @@ typedef struct
 
 
 int main(){
-    
-Car car1 = {"Porsche", 2025, 35000};
-Car car2 = {"Royce", 2022, 1000000};
-Car car3 = {"BMW", 2024, 250000};
 
-printf("%s %d $%d\n",car1.model, car1.year, car1.price);
-printf("%s %d $%d\n",car2.model, car2.year, car2.price);
-printf("%s %d $%d\n",car3.model, car3.year, car3.price);
+Car cars[] = { {"Porsche", 2025, 35000}, 
+{"Royce", 2022, 1000000}, 
+{"BMW", 2024, 250000}};
+     
+int num = sizeof(cars)/ sizeof(cars[0]);
+
+for(int i = 0; i < num; i ++){
+    printf("%s %d $%d\n",cars[1].model, cars[i].year, cars[i].price);
+    
+}
 
 
     return 0;
