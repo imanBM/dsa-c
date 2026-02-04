9726b90f8dbd8d79fb65fe574f2c0cd6049cd9c3
iman <mainaemmanuelmk@gmail.com>
2026-02-04 20:38:24 +0300

    Array of structs

--- dsa.c
+++ dsa.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+struct car
+{
+   char model[25];
+   int year;
+   int price;
+};
+
+
 int main(){
     printf("Hellooooooo Git\n");
     printf("yep....it just got real\n");
