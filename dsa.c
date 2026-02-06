8eabd1a61419b56217a2742506e6c95ed007a576
iman <mainaemmanuelmk@gmail.com>
2026-02-06 20:30:20 +0300

    CREATING a file in C

--- dsa.c
+++ dsa.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-void birthday(int* age);
-
 int main(){
 
-    int age = 25;
-    int *pAge = &age;
+    FILE *pFile = fopen("output.txt", "w");
 
+    char text[] = "SOUTHSIDE SOUTHside southside....uhhhhhhhhh!";
 
-    birthday(pAge);
+    if(pFile == NULL){
+        printf("Error opening file\n");
+        return 1;
 
-    printf("You are %d years old", age);
-    return 0;
-
-    
-} 
-void birthday(int* age){
-        (*age)++;
     }
+
+    fprintf(pFile,"%s", text);
+
+    printf("File was written successfully\n");
+
+    fclose(pFile);
+
+    return 0;
+}
